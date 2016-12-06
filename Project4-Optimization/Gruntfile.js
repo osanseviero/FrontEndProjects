edd3f2c39f8a24396d202b84538f2cdbacbe61d4
Omar S <osanseviero@gmail.com>
2016-12-06 15:45:01 -0600

    feat: Change the PageSpeed to more than 90

--- Project4-Optimization/Gruntfile.js
+++ Project4-Optimization/Gruntfile.js
@@ -1,1 +1,92 @@
-module.exports = function
+module.exports = function(grunt) {
+  grunt.initConfig({
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['css/*.css', 'views/css/*.css'],
+          dest: 'dest/',
+          ext: '.css'
+        }]
+      }
+    },
+    htmlmin: {                                    
+      dist: {                                      
+        options: {                                 
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   
+          'dest/index.html': 'src/index.html',                   
+          'dest/project-2048.html': 'src/project-2048.html',                   
+          'dest/project-mobile.html': 'src/project-mobile.html',                   
+          'dest/project-webperf.html': 'src/project-webperf.html',                   
+          'dest/views/pizza.html': 'src/views/pizza.html',                   
+        }
+      }
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 4
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.png'],
+            // Could also match cwd line above. i.e. project-directory/img/
+            dest: 'dest/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true,
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'src/',
+            src: ['**/*.jpg'],
+            // Could also match cwd. i.e. project-directory/img/
+            dest: 'dest/',
+            ext: '.jpg'
+          }
+        ]
+      }
+    },
+    uglify: {
+      target: {
+        files: {
+          'dest/js/perfmatters.js': 'src/js/perfmatters.js',
+          'dest/views/js/main.js': 'src/views/js/main.js',
+        }
+      }
+    },
+    inline: {
+      dist: {
+        src: 'dest/index.html',
+        dest: 'dest/index.html'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-inline');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin', 'uglify', 'htmlmin', 'imagemin', 'inline']);
+
+};
---
+++ Project4-Optimization/README.md
@@ -0,0 +1,7 @@
+* Removed blocking JavaScript like Analytics
+* Minify CSS
+* Minify HTML
+* Minified all images (jpgs and pngs with a level of 4/7)
+* Uglified the javascript
+* Loaded the font in CSS (only one stylesheet used it)
+* Inlined all css

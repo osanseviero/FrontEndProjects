e1fd335e80c4ca850998d7dfc56ef7093236da64
Omar S <osanseviero@gmail.com>
2016-12-22 22:29:07 -0500

    feat: Fill info window with Foursquare information

--- Project5-Map/js/app.js
+++ Project5-Map/js/app.js
@@ -16,6 +16,7 @@ function initMap() {
     {title: '500 Startups', location: {lat: 19.425414, lng: -99.162448}},
     {title: 'Nearsoft', location: {lat: 19.412991, lng: -99.164425}},
     {title: 'WeWork', location: {lat: 19.424371, lng: -99.167971}},
+    {title: 'Facebook', location: {lat: 19.426087, lng: -99.203319}}
   ];
 
   for(var i = 0; i < locations.length; i++) {
@@ -39,11 +40,40 @@ function initMap() {
 
 function populateInfoWindow(marker, infoWindow) {
   if(infoWindow.marker != marker) {
-      infoWindow.marker = marker;
-      infoWindow.setContent('<div>' + marker.title + '</div>');
-      infoWindow.open(map, marker);
-      infoWindow.addListener('cloclick', function() {
-          infoWindow.setMarker(null);
+      var formattedFSLink = getFoursquareInfo(marker.position.lat, marker.position.lng, function(data) {
+        infoWindow.marker = marker;
+        var content = '<h1>' + '<h1>' + marker.title + '</h1>'
+        console.log(data.twitter);
+        if(data.twitter) {
+          content += '<a href="https://twitter.com/' + data.twitter + '">Twitter</a>';
+        }
+        console.log(data.count);
+        content += "<h2>Count " + data.count + "</h2>";
+        infoWindow.setContent(content);
+        infoWindow.open(map, marker);
+        infoWindow.addListener('cloclick', function() {
+            infoWindow.setMarker(null);
+        });
       });
   }
 };
+
+
+/*Get twitter, phone, and count of a specific location*/
+function getFoursquareInfo(lat, lng, callback) {
+  var foursquareLink = 'https://api.foursquare.com/v2/venues/search?ll=%lat%,%lon%&client_id=FFNGZD4W0OKDN31OFQPYYHVUYL3KJJOZJ1OHV4LDBO0VW2OX&client_secret=ZQAKAY5RWOHWTFP0PM1LO5GNY00RIZMOCQAJOIHPO3YA1NPF&v=20120609';
+
+  var formattedFSLink = foursquareLink.replace('%lat%', lat);
+  formattedFSLink = formattedFSLink.replace('%lon%', lng);
+
+  $.getJSON(formattedFSLink).then(function(data){
+    console.log(data.response.venues[0]);
+    callback({
+      count: data.response.venues[0].stats.checkinsCount,
+      phone: data.response.venues[0].contact.photo,
+      twitter: data.response.venues[0].contact.twitter
+    });
+  });
+};
+
+

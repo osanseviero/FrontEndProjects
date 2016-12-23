a026c1efc88909e13355058b485a1e501635c97b
Omar S <osanseviero@gmail.com>
2016-12-22 23:21:36 -0500

    feat: Handle precision errors so Foursquare API selects correct venue

--- Project5-Map/js/app.js
+++ Project5-Map/js/app.js
@@ -11,12 +11,13 @@ function initMap() {
   var bounds = new google.maps.LatLngBounds();
 
   var locations = [
-    {title: 'Centraal', location: {lat: 19.412080, lng: -99.180513}},
-    {title: 'Google Mexico', location: {lat: 19.428222, lng: -99.206510}},
+    {title: 'Ceentral', location: {lat: 19.412080, lng: -99.180513}},
+    {title: 'Google México', location: {lat: 19.428222, lng: -99.206510}},
     {title: '500 Startups', location: {lat: 19.425414, lng: -99.162448}},
-    {title: 'Nearsoft', location: {lat: 19.412991, lng: -99.164425}},
-    {title: 'WeWork', location: {lat: 19.424371, lng: -99.167971}},
-    {title: 'Facebook', location: {lat: 19.426087, lng: -99.203319}}
+    {title: 'Nearsoft DF', location: {lat: 19.412991, lng: -99.164425}},
+    {title: 'WeWork Varsovia', location: {lat: 19.424371, lng: -99.167971}},
+    {title: 'Facebook', location: {lat: 19.426087, lng: -99.203319}},
+    {title: 'Centro de Cultura Digital', location: {lat: 19.423134, lng: -99.1763432}}
   ];
 
   for(var i = 0; i < locations.length; i++) {
@@ -40,14 +41,21 @@ function initMap() {
 
 function populateInfoWindow(marker, infoWindow) {
   if(infoWindow.marker != marker) {
-      var formattedFSLink = getFoursquareInfo(marker.position.lat, marker.position.lng, function(data) {
+      var formattedFSLink = getFoursquareInfo(marker.title, marker.position.lat, marker.position.lng, function(data) {
         infoWindow.marker = marker;
-        var content = '<h1>' + '<h1>' + marker.title + '</h1>'
-        console.log(data.twitter);
+        var content = '<h1>' + '<h1>' + marker.title + '</h1>';
+        console.log(data);
+        if(data === undefined) {
+          content += '<div>No data available for this location</div>';
+        }
         if(data.twitter) {
           content += '<a href="https://twitter.com/' + data.twitter + '">Twitter</a>';
         }
-        console.log(data.count);
+        if(data.phone) {
+          content += '<div>' + data.phone + '"</div>';
+
+        }
+       
         content += "<h2>Count " + data.count + "</h2>";
         infoWindow.setContent(content);
         infoWindow.open(map, marker);
@@ -60,19 +68,24 @@ function populateInfoWindow(marker, infoWindow) {
 
 
 /*Get twitter, phone, and count of a specific location*/
-function getFoursquareInfo(lat, lng, callback) {
+function getFoursquareInfo(title, lat, lng, callback) {
   var foursquareLink = 'https://api.foursquare.com/v2/venues/search?ll=%lat%,%lon%&client_id=FFNGZD4W0OKDN31OFQPYYHVUYL3KJJOZJ1OHV4LDBO0VW2OX&client_secret=ZQAKAY5RWOHWTFP0PM1LO5GNY00RIZMOCQAJOIHPO3YA1NPF&v=20120609';
 
   var formattedFSLink = foursquareLink.replace('%lat%', lat);
   formattedFSLink = formattedFSLink.replace('%lon%', lng);
 
   $.getJSON(formattedFSLink).then(function(data){
-    console.log(data.response.venues[0]);
-    callback({
-      count: data.response.venues[0].stats.checkinsCount,
-      phone: data.response.venues[0].contact.photo,
-      twitter: data.response.venues[0].contact.twitter
-    });
+    for(var i = 0; i < data.response.venues.length; i++) {
+      if(data.response.venues[i].name == title) {
+        callback({
+          count: data.response.venues[i].stats.checkinsCount,
+          phone: data.response.venues[i].contact.phone,
+          twitter: data.response.venues[i].contact.twitter
+        });
+        i = data.response.venues.length;
+      }
+    }
+    callback(undefined);
   });
 };
 
